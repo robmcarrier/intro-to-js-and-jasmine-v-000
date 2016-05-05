8cbe8088bc35f054019e3d0a9b06c7a48843ea34
Robert <robmcarrier@gmail.com>
2016-05-05 13:48:57 -0700

    Done.

--- code.js
+++ code.js
@@ -1,1 +1,16 @@
 'use strict';
+function favIceCream(flavor){
+  return "I love " + flavor;
+}
+
+function shouting(word){
+  return word.toUpperCase();
+}
+
+function roundDown(num){
+  return Math.floor(num);
+}
+
+function theTruth(){
+  return true;
+}
